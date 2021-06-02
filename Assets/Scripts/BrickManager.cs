062574f37a057bcbd3f7f71fca8d04d54e8120c5
Daniel Pierce <daniel.pierce.iv@gmail.com>
2021-06-01 19:11:30 -0700

    Implement brick randomization.

--- Assets/Scripts/BrickManager.cs
+++ Assets/Scripts/BrickManager.cs
@@ -7,10 +7,11 @@ public class BrickManager : MonoBehaviour
 {
     [SerializeField] private BrickPool brickPool;
     [SerializeField] private Transform[] spawnPoints;
-    //[SerializeField] private int lowerBrickSpawnLimitBuffer = 2;
-    //[SerializeField] private int upperBrickSpawnLimitBuffer = 1;
+    [SerializeField] private int lowerBrickSpawnLimitBuffer = 2;
+    [SerializeField] private int upperBrickSpawnLimitBuffer = 1;
 
     private int currentPhase = 1;
+    private List<Transform> selectedSpawnPoints = new List<Transform>();
     
     private void InitializeBrick(Brick brick, Vector3 position)
     {
@@ -21,7 +22,7 @@ public class BrickManager : MonoBehaviour
 
     public void SpawnBricks()
     {
-        foreach (var spawnPoint in spawnPoints)
+        foreach (var spawnPoint in ChooseBrickSpawnPoints())
         {
             InitializeBrick(brickPool.TakeObject(), spawnPoint.position);
         }
@@ -36,5 +37,27 @@ public class BrickManager : MonoBehaviour
         {
             if(brick.isActiveAndEnabled) brick.MoveDown();
         }
+    }
+
+    private List<Transform> ChooseBrickSpawnPoints()
+    {
+        selectedSpawnPoints.Clear();
+        selectedSpawnPoints.AddRange(spawnPoints);
+
+        // Selects a number between lowerBrickSpawnLimitBuffer and the total
+        // number of spawn points minus upperBrickSpawnLimitBuffer (both inclusive)
+        int numBricksToSpawn = UnityEngine.Random.Range(
+            lowerBrickSpawnLimitBuffer,
+            selectedSpawnPoints.Count - (upperBrickSpawnLimitBuffer - 1));
+
+        int numBricksToEliminate = selectedSpawnPoints.Count - numBricksToSpawn;
+
+        for (int i = 0; i < numBricksToEliminate; i++)
+        {
+            selectedSpawnPoints.RemoveAt(
+                UnityEngine.Random.Range(0, selectedSpawnPoints.Count));
+        }
+
+        return selectedSpawnPoints;
     }
 }
